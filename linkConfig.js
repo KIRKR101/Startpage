d7d2091a028a7dde96e7e0b292ff697a366c7da1
KIRKR101 <104659112+KIRKR101@users.noreply.github.com>
2024-04-06 01:25:25 +0100

    Add files

---
+++ linkConfig.js
@@ -0,0 +1,98 @@
+const linkConfig = [
+    {
+        category: "social/",
+        links: [
+            {
+                showAs: "twitter",
+                link: "https://www.twitter.com"
+            },
+            {
+                showAs: "reddit",
+                link: "https://www.reddit.com"
+            },
+            {
+                showAs: "instagram",
+                link: "https://www.instagram.com"
+            },
+            {
+                showAs: "wikipedia",
+                link: "https://en.wikipedia.org/wiki/Main_Page"
+            },
+            {
+                showAs: "emails",
+                link: "https://mail.google.com/mail/u/0/#inbox"
+            },
+
+        ],
+    },
+    {
+        category: "media/",
+        links: [
+            {
+                showAs: "youtube",
+                link: "https://www.youtube.com"
+            },
+            {
+                showAs: "twitch",
+                link: "https://www.twitch.tv"
+            },
+            {
+                showAs: "spotify",
+                link: "https://open.spotify.com"
+            },
+            {
+                showAs: "real-debrid",
+                link: "https://www.real-debrid.com"
+            },
+	    {
+                showAs: "stremio",
+                link: "https://web.stremio.com/"
+            },
+
+        ],
+    },
+    {
+        category: "piracy/",
+        links: [
+            {
+                showAs: "rarbg",
+                link: "https://www.thrarbg.com"
+            },
+            {
+                showAs: "megathread",
+                link: "https://rentry.org/megathread"
+            },
+            {
+                showAs: "bitsearch",
+                link: "https://torrentgalaxy.to"
+            },
+            {
+                showAs: "rarbg index",
+                link: "https://rarbg.chatgptserver.workers.dev/"
+            },
+	    {
+                showAs: "general downloader",
+                link: "https://cobalt.tools/"
+            },
+
+        ],
+    },
+    {
+        category: "revision/",
+        links: [
+            {
+                showAs: "12ft",
+                link: "https://www.12ft.io"
+            },
+            {
+                showAs: "sme",
+                link: "https://savemyexams.com"
+            },
+            {
+                showAs: "drive",
+                link: "https://drive.google.com"
+            },
+
+        ],
+    },
+]
